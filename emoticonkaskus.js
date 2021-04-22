562adef24bec5fb54cd20c2658ee12c4461eb696
Moiseev Aleksej <moiseev.003@bk.ru>
2021-04-22 17:08:19 +0300

    add JS script

---
+++ emoticonkaskus.js
@@ -0,0 +1,118 @@
+a = document.getElementById('comments');
+if(a) {
+	b = a.getElementsByTagName("DD");
+	for(i=0; i < b.length; i++) {
+            if (b.item(i).getAttribute('CLASS') == 'comment-body') {
+                _str = b.item(i).innerHTML.replace(/:10/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULISAgyI/AAAAAAAABxs/bVLPv-CmDRA/kaskuser_t4belajarblogger_01.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:11/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FULLTi5zI/AAAAAAAABxw/a18PvE6DMz8/kaskuser_t4belajarblogger_02.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:12/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULQB_34I/AAAAAAAABx0/CHYgHAJ0RME/kaskuser_t4belajarblogger_03.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:13/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FULotxxDI/AAAAAAAABx4/NCjzQEXq0D8/kaskuser_t4belajarblogger_04.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:14/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULnLi7pI/AAAAAAAABx8/zbPOah3ivGs/kaskuser_t4belajarblogger_05.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:15/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVE4voLDI/AAAAAAAAByA/YBHkp8ydDNI/kaskuser_t4belajarblogger_06.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:16/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVE9toN3I/AAAAAAAAByE/EI9xHHgNVb0/kaskuser_t4belajarblogger_07.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:17/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVFA7abFI/AAAAAAAAByI/8UILEDSz4_M/kaskuser_t4belajarblogger_08.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:18/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FVFWdwpBI/AAAAAAAAByM/bVVvVCP5Ap0/kaskuser_t4belajarblogger_09.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:19/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVFk_KY1I/AAAAAAAAByQ/12i2qYuBWPc/kaskuser_t4belajarblogger_10.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:20/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVZRNiD1I/AAAAAAAAByU/A_ahrCm6l6U/kaskuser_t4belajarblogger_11.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:21/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVZclphZI/AAAAAAAAByY/sKmT5BAPbBU/kaskuser_t4belajarblogger_12.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:22/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVZWIp2MI/AAAAAAAAByc/4TWBttgCAT4/kaskuser_t4belajarblogger_13.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:23/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVZvvO5AI/AAAAAAAAByg/D2jMwrdkCS0/kaskuser_t4belajarblogger_14.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:24/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVZjpq0LI/AAAAAAAAByk/dElFDnJJ_fA/kaskuser_t4belajarblogger_15.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:25/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVoQzxquI/AAAAAAAAByo/SFSJaRx2lbM/kaskuser_t4belajarblogger_16.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:26/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVohvRi6I/AAAAAAAABys/O4WgQHkgVPc/kaskuser_t4belajarblogger_17.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:27/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVombpjmI/AAAAAAAAByw/btR9_27WZUs/kaskuser_t4belajarblogger_18.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:28/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVow8ocqI/AAAAAAAABy0/0r-1r-xD-nU/kaskuser_t4belajarblogger_19.gif' alt='' class='smiley'/>");  
+                _str = _str.replace(/:29/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVo_f79mI/AAAAAAAABy4/rwPLUuBSNpE/kaskuser_t4belajarblogger_20.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:30/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FXwen5qPI/AAAAAAAABy8/s3hhOLnj1dU/kaskuser_t4belajarblogger_21.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:31/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FXwn4fp6I/AAAAAAAABzA/Es3zlyr7y3M/kaskuser_t4belajarblogger_22.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:32/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FXwoweVmI/AAAAAAAABzE/IAA1HXI53N0/kaskuser_t4belajarblogger_23.gif' alt='' class='smiley'/>")
+                _str = _str.replace(/:33/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FXw6cEPiI/AAAAAAAABzI/UbU-PvgOPGI/kaskuser_t4belajarblogger_24.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:34/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FXxOH6z9I/AAAAAAAABzM/A6RDSqs9pP4/kaskuser_t4belajarblogger_25.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:35/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYBbV5L2I/AAAAAAAABzQ/7bwqMJduKB4/kaskuser_t4belajarblogger_26.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:36/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYBg-woEI/AAAAAAAABzU/-elKYvDyFJE/kaskuser_t4belajarblogger_27.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:37/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYBoDHUFI/AAAAAAAABzY/jlT9AzaQ5q0/kaskuser_t4belajarblogger_28.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:38/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYB-Jj1JI/AAAAAAAABzc/5unPe41xJ00/kaskuser_t4belajarblogger_29.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:39/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYBxdE3GI/AAAAAAAABzg/oeX35Qw6PTw/kaskuser_t4belajarblogger_30.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:40/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYVeP80pI/AAAAAAAABzk/SXQQ98AhjKM/kaskuser_t4belajarblogger_31.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:41/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYVpDd93I/AAAAAAAABzo/1rgwJPAwrbY/kaskuser_t4belajarblogger_32.gif' alt='' class='smiley'/>");  
+                _str = _str.replace(/:42/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYVqbW8QI/AAAAAAAABzs/a_OfH43__dU/kaskuser_t4belajarblogger_33.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:43/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYV4O2HjI/AAAAAAAABzw/kCZYiA-9vvg/kaskuser_t4belajarblogger_34.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:44/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYWI2LNOI/AAAAAAAABz0/OvIM69lsJWo/kaskuser_t4belajarblogger_35.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:45/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYjSQOewI/AAAAAAAAB0A/xCYOO6KTJw0/kaskuser_t4belajarblogger_36.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:46/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYjrSGyKI/AAAAAAAAB0E/SpH5mEfiO-M/kaskuser_t4belajarblogger_37.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:47/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYjj055PI/AAAAAAAAB0I/HTOO9uo2HGU/kaskuser_t4belajarblogger_38.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:48/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYj0CU_8I/AAAAAAAAB0M/Go1vSM91ITs/kaskuser_t4belajarblogger_39.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:49/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYj8wYbKI/AAAAAAAAB0Q/jABAjcHxMVY/kaskuser_t4belajarblogger_40.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:50/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYw1f19OI/AAAAAAAAB0U/q1Focvgt8Z4/kaskuser_t4belajarblogger_41.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:51/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYxJIHXHI/AAAAAAAAB0Y/FVII5Spn1_4/kaskuser_t4belajarblogger_42.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:52/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYxMTm3zI/AAAAAAAAB0c/dtIO8LlQfts/kaskuser_t4belajarblogger_43.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:53/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYxRR829I/AAAAAAAAB0g/zy1Mpyv13t4/kaskuser_t4belajarblogger_44.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:54/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYxgxW_uI/AAAAAAAAB0k/iiaVqJ102LQ/kaskuser_t4belajarblogger_45.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:55/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FY-6uLVuI/AAAAAAAAB0o/XuK4E0HmF2Y/kaskuser_t4belajarblogger_46.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:56/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FY_ChnSMI/AAAAAAAAB0s/_9S6PgjAr_w/kaskuser_t4belajarblogger_47.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:57/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FY_BcYw7I/AAAAAAAAB0w/IUNl3mf6MZg/kaskuser_t4belajarblogger_48.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:58/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FY_Y0juYI/AAAAAAAAB00/Rs84gbdF86I/kaskuser_t4belajarblogger_49.gif' alt='' class='smiley'/>")
+
+                b.item(i).innerHTML = _str;
+            }
+	}
+}
+
+a = document.getElementById('comments');
+if(a) {
+	c = a.getElementsByTagName("DD");
+	for(i=0; i < c.length; i++) {
+            if (c.item(i).getAttribute('CLASS') == 'comment-body-author') {
+                _str = c.item(i).innerHTML.replace(/:10/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULISAgyI/AAAAAAAABxs/bVLPv-CmDRA/kaskuser_t4belajarblogger_01.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:11/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FULLTi5zI/AAAAAAAABxw/a18PvE6DMz8/kaskuser_t4belajarblogger_02.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:12/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULQB_34I/AAAAAAAABx0/CHYgHAJ0RME/kaskuser_t4belajarblogger_03.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:13/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FULotxxDI/AAAAAAAABx4/NCjzQEXq0D8/kaskuser_t4belajarblogger_04.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:14/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FULnLi7pI/AAAAAAAABx8/zbPOah3ivGs/kaskuser_t4belajarblogger_05.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:15/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVE4voLDI/AAAAAAAAByA/YBHkp8ydDNI/kaskuser_t4belajarblogger_06.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:16/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVE9toN3I/AAAAAAAAByE/EI9xHHgNVb0/kaskuser_t4belajarblogger_07.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:17/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVFA7abFI/AAAAAAAAByI/8UILEDSz4_M/kaskuser_t4belajarblogger_08.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:18/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FVFWdwpBI/AAAAAAAAByM/bVVvVCP5Ap0/kaskuser_t4belajarblogger_09.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:19/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVFk_KY1I/AAAAAAAAByQ/12i2qYuBWPc/kaskuser_t4belajarblogger_10.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:20/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVZRNiD1I/AAAAAAAAByU/A_ahrCm6l6U/kaskuser_t4belajarblogger_11.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:21/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVZclphZI/AAAAAAAAByY/sKmT5BAPbBU/kaskuser_t4belajarblogger_12.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:22/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVZWIp2MI/AAAAAAAAByc/4TWBttgCAT4/kaskuser_t4belajarblogger_13.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:23/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVZvvO5AI/AAAAAAAAByg/D2jMwrdkCS0/kaskuser_t4belajarblogger_14.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:24/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FVZjpq0LI/AAAAAAAAByk/dElFDnJJ_fA/kaskuser_t4belajarblogger_15.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:25/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVoQzxquI/AAAAAAAAByo/SFSJaRx2lbM/kaskuser_t4belajarblogger_16.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:26/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVohvRi6I/AAAAAAAABys/O4WgQHkgVPc/kaskuser_t4belajarblogger_17.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:27/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVombpjmI/AAAAAAAAByw/btR9_27WZUs/kaskuser_t4belajarblogger_18.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:28/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FVow8ocqI/AAAAAAAABy0/0r-1r-xD-nU/kaskuser_t4belajarblogger_19.gif' alt='' class='smiley'/>");  
+                _str = _str.replace(/:29/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FVo_f79mI/AAAAAAAABy4/rwPLUuBSNpE/kaskuser_t4belajarblogger_20.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:30/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FXwen5qPI/AAAAAAAABy8/s3hhOLnj1dU/kaskuser_t4belajarblogger_21.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:31/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FXwn4fp6I/AAAAAAAABzA/Es3zlyr7y3M/kaskuser_t4belajarblogger_22.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:32/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FXwoweVmI/AAAAAAAABzE/IAA1HXI53N0/kaskuser_t4belajarblogger_23.gif' alt='' class='smiley'/>")
+                _str = _str.replace(/:33/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FXw6cEPiI/AAAAAAAABzI/UbU-PvgOPGI/kaskuser_t4belajarblogger_24.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:34/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FXxOH6z9I/AAAAAAAABzM/A6RDSqs9pP4/kaskuser_t4belajarblogger_25.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:35/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYBbV5L2I/AAAAAAAABzQ/7bwqMJduKB4/kaskuser_t4belajarblogger_26.gif' alt='' class='smiley'/>");
+                _str = _str.replace(/:36/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYBg-woEI/AAAAAAAABzU/-elKYvDyFJE/kaskuser_t4belajarblogger_27.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:37/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYBoDHUFI/AAAAAAAABzY/jlT9AzaQ5q0/kaskuser_t4belajarblogger_28.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:38/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYB-Jj1JI/AAAAAAAABzc/5unPe41xJ00/kaskuser_t4belajarblogger_29.gif' alt='' class='smiley'/>"); 
+                _str = _str.replace(/:39/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYBxdE3GI/AAAAAAAABzg/oeX35Qw6PTw/kaskuser_t4belajarblogger_30.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:40/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYVeP80pI/AAAAAAAABzk/SXQQ98AhjKM/kaskuser_t4belajarblogger_31.gif' alt='' class='smiley'/>");
+		_str = _str.replace(/:41/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYVpDd93I/AAAAAAAABzo/1rgwJPAwrbY/kaskuser_t4belajarblogger_32.gif' alt='' class='smiley'/>");  
+                _str = _str.replace(/:42/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYVqbW8QI/AAAAAAAABzs/a_OfH43__dU/kaskuser_t4belajarblogger_33.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:43/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYV4O2HjI/AAAAAAAABzw/kCZYiA-9vvg/kaskuser_t4belajarblogger_34.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:44/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYWI2LNOI/AAAAAAAABz0/OvIM69lsJWo/kaskuser_t4belajarblogger_35.gif' alt='' class='smiley'/>"); 
+		_str = _str.replace(/:45/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYjSQOewI/AAAAAAAAB0A/xCYOO6KTJw0/kaskuser_t4belajarblogger_36.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:46/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYjrSGyKI/AAAAAAAAB0E/SpH5mEfiO-M/kaskuser_t4belajarblogger_37.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:47/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYjj055PI/AAAAAAAAB0I/HTOO9uo2HGU/kaskuser_t4belajarblogger_38.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:48/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYj0CU_8I/AAAAAAAAB0M/Go1vSM91ITs/kaskuser_t4belajarblogger_39.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:49/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYj8wYbKI/AAAAAAAAB0Q/jABAjcHxMVY/kaskuser_t4belajarblogger_40.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:50/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYw1f19OI/AAAAAAAAB0U/q1Focvgt8Z4/kaskuser_t4belajarblogger_41.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:51/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FYxJIHXHI/AAAAAAAAB0Y/FVII5Spn1_4/kaskuser_t4belajarblogger_42.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:52/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FYxMTm3zI/AAAAAAAAB0c/dtIO8LlQfts/kaskuser_t4belajarblogger_43.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:53/gi, "<img src='http://lh3.ggpht.com/_RVpTV2JOOxA/S1FYxRR829I/AAAAAAAAB0g/zy1Mpyv13t4/kaskuser_t4belajarblogger_44.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:54/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FYxgxW_uI/AAAAAAAAB0k/iiaVqJ102LQ/kaskuser_t4belajarblogger_45.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:55/gi, "<img src='http://lh4.ggpht.com/_RVpTV2JOOxA/S1FY-6uLVuI/AAAAAAAAB0o/XuK4E0HmF2Y/kaskuser_t4belajarblogger_46.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:56/gi, "<img src='http://lh5.ggpht.com/_RVpTV2JOOxA/S1FY_ChnSMI/AAAAAAAAB0s/_9S6PgjAr_w/kaskuser_t4belajarblogger_47.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:57/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FY_BcYw7I/AAAAAAAAB0w/IUNl3mf6MZg/kaskuser_t4belajarblogger_48.gif' alt='' class='smiley'/>")
+		_str = _str.replace(/:58/gi, "<img src='http://lh6.ggpht.com/_RVpTV2JOOxA/S1FY_Y0juYI/AAAAAAAAB00/Rs84gbdF86I/kaskuser_t4belajarblogger_49.gif' alt='' class='smiley'/>")
+                b.item(i).innerHTML = _str;
+            }
+	}
+}
